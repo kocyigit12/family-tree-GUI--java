d324beefbddf930cfd7618211e884629e6cba2ca
= <=>
2022-01-11 22:07:17 +0300

    updating FamilyReaderWriter , adding a method to open family tree by scanning a chosen file

--- src/File_IO/FamilyReaderWriter.java
+++ src/File_IO/FamilyReaderWriter.java
@@ -1,8 +1,12 @@
 package File_IO;
 
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import model.*;
+import view.FamilyView;
 import view.FamilyView.Coordinates;
 
 
@@ -30,7 +34,84 @@ public class FamilyReaderWriter {
         return coordinates;
     }
 
+    public void familyOpen(File file) throws IOException {
+        FileReader fr = new FileReader(file);
+        Scanner treeScanner = new Scanner(fr).useDelimiter("\r\n");
+        Scanner personScanner;
+        Scanner childrenScanner;
+        String name = null;
+        int age = 0;
+        LinkedList<String> children = new LinkedList();
+        LinkedList<String> parents = new LinkedList();
+        String spouse = null;
+        int level = 0;
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+        FamilyView view = new FamilyView();
+        while (treeScanner.hasNextLine()) {
+            name = null;
+            age = 0;
+            children = new LinkedList();
+            parents = new LinkedList();
+            spouse = null;
+            level = 0;
+            x = 0;
+            y = 0;
+            width = 0;
+            height = 0;
+            personScanner = new Scanner(treeScanner.nextLine()).useDelimiter("[.]");
+            if (personScanner.hasNext()) {
+                name = personScanner.next();
+            } else {
+                return;
+            }
+            if (personScanner.hasNextInt()) {
+                age = personScanner.nextInt();
+            } else {
+                return;
+            }
+            if (personScanner.hasNext()) {
+                childrenScanner = new Scanner(personScanner.next()).useDelimiter("[,]");;
+                while (childrenScanner.hasNext()) {
+                    children.add(childrenScanner.next());
+                }
 
+            }
+            if (personScanner.hasNext()) {
+                childrenScanner = new Scanner(personScanner.next()).useDelimiter("[,]");
+                while (childrenScanner.hasNext()) {
+                    parents.add(childrenScanner.next());
+                }
+            }
+            if (personScanner.hasNext()) {
+                spouse = personScanner.next();
+            }
+            if (personScanner.hasNextInt()) {
+                level = personScanner.nextInt();
+            } else {
+                return;
+            }
+            if (personScanner.hasNextInt()) {
+                x = personScanner.nextInt();
+            }
+            if (personScanner.hasNextInt()) {
+                y = personScanner.nextInt();
+            }
+            if (personScanner.hasNextInt()) {
+                width = personScanner.nextInt();
+            }
+            if (personScanner.hasNextInt()) {
+                height = personScanner.nextInt();
+            }
+            tree.add(new Person(name, age, children, parents, spouse, level));
+            view.changePersonCoords(name, x, y, width, height);
+            coordinates = view.getFamilyViewCoordinates();
+
+        }
+
+    }
 
 
 }
